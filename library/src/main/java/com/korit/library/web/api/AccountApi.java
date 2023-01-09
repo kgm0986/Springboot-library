febd02f3e3d0ead1f9fd920a4c49360b785634a2
kgm0986 <kgm5371@naver.com>
2023-01-09 20:52:28 +0900

    어우 오랜만 ㅇ거 회원가입까지 구현했고 다음은 api명세서

---
+++ library/src/main/java/com/korit/library/web/api/AccountApi.java
@@ -0,0 +1,39 @@
+package com.korit.library.web.api;
+
+import com.korit.library.aop.annotation.ValidAspect;
+import com.korit.library.service.AccountService;
+import com.korit.library.web.dto.CMRespDto;
+import com.korit.library.web.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountApi {
+
+    @Autowired
+    private AccountService accountService;
+
+    @ValidAspect
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+
+        accountService.duplicateUsername(userDto.getUsername());
+        accountService.compareToPassword(userDto.getPassword(),userDto.getRepassword());
+
+        UserDto user = accountService.registerUser(userDto);
+
+        return ResponseEntity
+                .created(URI.create("/api/account/user/"+user.getUserId()))
+                .body(new CMRespDto<>("Create a new User", user));
+    }
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable int userId) {
+        return ResponseEntity.ok().body(new CMRespDto<>("Success",accountService.gerUser(userId)));
+    }
+}
