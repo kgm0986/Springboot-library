6c155ed75537dc636d6e75c3381b0051f0f953e5
kgm0986 <kgm5371@naver.com>
2023-01-10 22:04:43 +0900

    로그인 기능 완성 로그인 권한 확인 가능

--- library/src/main/java/com/korit/library/web/api/AccountApi.java
+++ library/src/main/java/com/korit/library/web/api/AccountApi.java
@@ -1,18 +1,23 @@
 package com.korit.library.web.api;
 
 import com.korit.library.aop.annotation.ValidAspect;
+import com.korit.library.security.PrincipalDeteils;
 import com.korit.library.service.AccountService;
 import com.korit.library.web.dto.CMRespDto;
 import com.korit.library.web.dto.UserDto;
+import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
-
+@Slf4j
+@Api(tags = ("Account Rest API Controller"))//스웨거 이름정하는거
 @RestController
 @RequestMapping("/api/account")
 public class AccountApi {
@@ -20,9 +25,11 @@ public class AccountApi {
     @Autowired
     private AccountService accountService;
 
+    @ApiOperation(value = "회원가입",notes = "뢰원가입 요청 메소드")//http://localhost:8000/swagger-ui/#/Account%20Rest%20API%20Controller
+
     @ValidAspect
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+    public ResponseEntity<? extends CMRespDto<? extends UserDto>> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
 
         accountService.duplicateUsername(userDto.getUsername());
         accountService.compareToPassword(userDto.getPassword(),userDto.getRepassword());
@@ -33,8 +40,30 @@ public class AccountApi {
                 .created(URI.create("/api/account/user/"+user.getUserId()))
                 .body(new CMRespDto<>(HttpStatus.CREATED.value(),"Create a new User", user));
     }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "사용자 식별 코드",required = true,dataType = "int")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "클라이언트가 잘못했음"), //위에 링크 눌러봐라
+            @ApiResponse(code = 400, message = "클라이언트가 잘못했음2")//위에 링크 눌러봐라
+    })
+
+
     @GetMapping("/user/{userId}")
-    public ResponseEntity<?> getUser(@PathVariable int userId) {
+    public ResponseEntity<? extends CMRespDto<? extends UserDto>> getUser(
+       //     @ApiParam(value = "사용자 식별 코드" )
+            @PathVariable int userId) {
         return ResponseEntity.ok().body(new CMRespDto<>(HttpStatus.OK.value(), "Success",accountService.gerUser(userId)));
+    }
+    @ApiOperation(value = "Get Principal",notes = "로그인된 사용자 정보 가져오기")
+    @GetMapping("/principal")
+    public ResponseEntity<CMRespDto<? extends PrincipalDeteils>> getPrincipalDeteils(@ApiParam(name = "PrincipalDeteils",hidden = true) @AuthenticationPrincipal PrincipalDeteils principalDeteils) {
+        principalDeteils.getAuthorities().forEach(role -> {
+            log.info("로그인된 사용자의 권한 :{}",role.getAuthority());
+        });
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Success",principalDeteils));
     }
 }
--- library/src/main/java/com/korit/library/web/dto/UserDto.java
+++ library/src/main/java/com/korit/library/web/dto/UserDto.java
@@ -1,9 +1,11 @@
 package com.korit.library.web.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -15,23 +17,32 @@ import java.util.List;
 @AllArgsConstructor
 @Data
 public class UserDto {
+
     private int userId;
 
     @NotBlank
+    @ApiModelProperty(name = "username",value = "사용자이름",example = "abc",required = true)
     private String username;
+    @ApiModelProperty(name = "password",value = "비번",example = "1234",required = true)
     @NotBlank
     private String password;
     @NotBlank
+    @ApiModelProperty(name = "repassword",value = "비번확인",example = "1234",required = true)
     private String repassword;
 
     @NotBlank
+    @ApiModelProperty(name = "name",value = "이름",example = "김규민",required = true)
     private String name;
     @NotBlank
+    @ApiModelProperty(name = "email",value = "이메일",example = "kgm0986@naver.com",required = true)
     @Email
     private String email;
 
+    @ApiModelProperty(hidden = true)
     private List<RoleDtlDto> roleDtlDto;
 
+    @ApiModelProperty(hidden = true)
     private LocalDateTime createDate;
+    @ApiModelProperty(hidden = true)
     private LocalDateTime updateDate;
 }
