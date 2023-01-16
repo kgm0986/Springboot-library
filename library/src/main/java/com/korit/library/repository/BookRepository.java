5f26f26f75ab386209dcab0ff619b373ecdbf58f
kgm0986 <kgm5371@naver.com>
2023-01-16 21:54:08 +0900

    사진등록 및UUID를이용한 파일 덮어쓰기방지 등의 기능구현 복습 ㄱㄱ

--- library/src/main/java/com/korit/library/repository/BookRepository.java
+++ library/src/main/java/com/korit/library/repository/BookRepository.java
@@ -1,9 +1,6 @@
 package com.korit.library.repository;
 
-import com.korit.library.web.dto.BookMstDto;
-import com.korit.library.web.dto.BookReqDto;
-import com.korit.library.web.dto.CategoryDto;
-import com.korit.library.web.dto.SearchReqDto;
+import com.korit.library.web.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -34,4 +31,10 @@ public interface BookRepository {
     public int saveBook(BookReqDto bookReqDto);
 
     public int updateBookByBookCode(BookReqDto bookReqDto);
+
+    public int maintainUpdateBookByBookCode(BookReqDto bookReqDto);
+
+    public int deletBook(String bookCode);
+
+    public int registerBookImages(List<BookImageDto> bookImageDtos);
 }
--- library/src/main/java/com/korit/library/web/api/admin/BookApi.java
+++ library/src/main/java/com/korit/library/web/api/admin/BookApi.java
@@ -10,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Api(tags = {"관리자 도서관리 API"})
@@ -56,6 +58,31 @@ public class BookApi {
         return ResponseEntity
                 .ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+    @ValidAspect
+    @ParamsAspect
+    @PatchMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> removeBook(@PathVariable String bookCode, @Valid @RequestBody BookReqDto bookReqDto, BindingResult bindingResult) {
+        bookService.maintainModifyBook(bookReqDto);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(), "Successfully", true));
+    }
+    @ParamsAspect
+    @DeleteMapping("/book/{bookCode}")
+    public ResponseEntity<CMRespDto<?>> removeBook(@PathVariable String bookCode) {
+        bookService.removeBook(bookCode);
+        return ResponseEntity
+                .ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(),"Successfully", true));
+    }
+    @ParamsAspect
+    @PostMapping("/book/{bookCode}/images")
+    public ResponseEntity<CMRespDto<?>> registerBookImg(@PathVariable String bookCode ,@RequestPart List<MultipartFile> files) {//list로 걸어서 여러개 받기가능
+       bookService.registerBookImages(bookCode,files);
+        //System.out.println(file.getOriginalFilename());//파일의 실제이름 출력
+        return ResponseEntity.ok()
+                .body(new CMRespDto<>(HttpStatus.OK.value(),"Successfully",true));
     }
 
 
