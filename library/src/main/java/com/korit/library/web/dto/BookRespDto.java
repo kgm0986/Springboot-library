db3d3c5d55b9a4b64834ce9dcb68ac7f5762091f
kgm0986 <kgm5371@naver.com>
2023-01-20 21:57:23 +0900

    도서 관리시스템 관리자 페이지 제작중

---
+++ library/src/main/java/com/korit/library/web/dto/BookRespDto.java
@@ -0,0 +1,10 @@
+package com.korit.library.web.dto;
+
+import com.korit.library.entity.BookMst;
+
+import java.awt.print.Book;
+
+public class BookRespDto {
+    private BookMst bookMst;
+    private boolean
+}
